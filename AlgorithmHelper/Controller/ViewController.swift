118467e69530780db9e9d7bf60fcc1c9e1313c87
Eric Liu <ericliu314@gmail.com>
2018-01-16 10:41:33 -0500

    Initial commit

---
+++ AlgorithmHelper/Controller/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AlgorithmHelper
+//
+//  Created by Eric Liu on 2018-01-16.
+//  Copyright © 2018 Eric Liu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
